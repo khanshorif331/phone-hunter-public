6e725cc9309c18e0d58ad55f33cb7d98f95abc21
khanshorif331 <khanshorif331@gmail.com>
2022-02-28 21:48:55 +0600

    data load functionality added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
-console.log('hello baby')
+// loading data from the api 
+const loadData = ()=>{
+    const input = document.getElementById('input-field')
+    const inputValue = input.value
+    // console.log(inputValue);
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayData(data.data))
+    input.value = ''
+}
+
+// display data 
+
+const displayData = (phones) =>{
+    const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML =''
+    phones.forEach(phone=> {
+        
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML =`
+                <div class="card p-3" >
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${phone.phone_name}</h5>
+                      <p class="card-text">${phone.brand}</p>
+                    </div>
+                    <button id="details-button" class="btn btn-outline-success">See Details</button>
+                </div>
+        `
+        cardContainer.appendChild(div)
+        console.log(phone)
+        
+    })
+    
+}
+
+
+
