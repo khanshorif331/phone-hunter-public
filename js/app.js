03d808f5894a1e3ad66b05c7f5c9e00279f3af21
khanshorif331 <khanshorif331@gmail.com>
2022-02-28 22:15:20 +0600

    function added on details button

--- js/app.js
+++ js/app.js
@@ -13,10 +13,10 @@ const loadData = ()=>{
 // display data 
 
 const displayData = (phones) =>{
+    // console.log(phones.length) 
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML =''
-    phones.forEach(phone=> {
-        
+    phones.forEach(phone=> {       
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML =`
@@ -26,14 +26,29 @@ const displayData = (phones) =>{
                       <h5 class="card-title">${phone.phone_name}</h5>
                       <p class="card-text">${phone.brand}</p>
                     </div>
-                    <button id="details-button" class="btn btn-outline-success">See Details</button>
+                    <button onclick="getDetails('${phone.slug}')" id="details-button" class="btn btn-outline-success">See Details</button>
                 </div>
         `
-        cardContainer.appendChild(div)
-        console.log(phone)
-        
-    })
-    
+// console.log(phone.slug);    
+        cardContainer.appendChild(div)        
+    })   
+}
+
+
+// again loading data from api for details button
+
+const getDetails = phoneId =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data))
+    // console.log(phoneId)
+}
+
+// showing details data on the ui
+
+const displayDetails = item =>{
+    console.log(item);
 }
 
 
